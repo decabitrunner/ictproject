9af2a36292af7f4c2baaa64b64606a8da1274484
P_88 <peter.blazejx@gmail.com>
2023-06-10 15:29:49 +0200

    added sfx and score

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 const startButton = document.getElementById('start-btn')
 startButton.addEventListener('click', start)
 const questionContainer = document.getElementById('question-container')
+const scoreElement = document.getElementById('score')
 const questionElement = document.getElementById('question')
 const answerButtons = document.getElementById('answer-buttons')
+const scoreButton = document.getElementById('score-btn')
+scoreButton.addEventListener('click', showScore)
 const nextButton = document.getElementById('next-btn')
 nextButton.addEventListener('click', ()=> {
     setNext()
     currentQuestion++
 })
 
-const questions = []
-let shuffledQuestions, currentQuestion
+
+let questions = []
+let shuffledQuestions, currentQuestion, score
+
+let correctAudio = new Audio('/sfx/correct.wav')
+let wrongAudio = new Audio('/sfx/wrong.wav')
 
 
 function makeQuestions(){
+    questions = []
+    score = 0
     for (let i = 0; i<=10; i++){
         let a = Math.floor((Math.random()*12)+1)
         let b = Math.floor((Math.random()*12)+1)
@@ -31,11 +40,12 @@ function makeQuestions(){
     }
 }
 
-console.log(questions)
+
 
 
 
 function start() {
+    shuffledQuestions = []
     makeQuestions()
     resetstate()
     startButton.classList.add('hide')
@@ -76,25 +86,46 @@ function selectAns(e){
     Array.from(answerButtons.children).forEach(button=>{
         setStatusClass(button, button.dataset.correct)
     })
+    if(correct){
+        correctAudio.play()
+        score++
+    }else{
+        wrongAudio.play()
+    }
+    
     if (shuffledQuestions.length>currentQuestion+1){
         nextButton.classList.remove('hide')
         
     }else{
-        startButton.innerText = 'Restart'
-        startButton.classList.remove('hide')
-        makeQuestions()
+        
+        scoreButton.classList.remove('hide')
+        
         
     }
 
     
 }
 
+function showScore(){
+    clearStatusClass(document.body)
+    
+    questionContainer.classList.add('hide')
+    scoreElement.innerText = 'Score: '+score + '/'+shuffledQuestions.length
+    scoreElement.classList.remove('hide')
+    scoreButton.classList.add('hide')
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+   
+}
+
 function setStatusClass(element, correct){
     clearStatusClass(element)
     if (correct){
         element.classList.add('correct')
+        
     }else{
         element.classList.add('wrong')
+        
     }
 }
 
@@ -102,14 +133,17 @@ function clearStatusClass(element){
     element.classList.remove('correct')
     element.classList.remove('wrong')
 }
-
+ 
 
 function resetstate(){
     
     clearStatusClass(document.body)
     nextButton.classList.add('hide')
+    scoreButton.classList.add('hide')
+    scoreElement.classList.add('hide')
+    questionContainer.classList.remove('hide')
     while (answerButtons.firstElementChild){
-        console.log('removed 1 child')
+        
         answerButtons.removeChild(answerButtons.firstChild)
     }
 }
